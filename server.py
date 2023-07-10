ceac62916ab4bb460a1d341b34ea131ba14778cd
я люблю сов <136844294+Darknightoowl@users.noreply.github.com>
2023-07-10 19:06:40 +0500

    Add files via upload

--- server.py
+++ server.py
@@ -1,56 +1,26 @@
-from flask import Flask, request, render_template
-import sqlite3
-
-app = Flask(__name__)
-
-@app.route('/authorization', methods=['GET', 'POST'])
-def form_authorization():
-   if request.method == 'POST':
-       Login = request.form.get('Login')
-       Password = request.form.get('Password')
-
-       db_lp = sqlite3.connect(login_password.db')
-       cursor_db = db_lp.cursor()
-       cursor_db.execute(('''SELECT password FROM passwords
-                                               WHERE login = '{}';
-                                               ''').format(Login))
-       pas = cursor_db.fetchall()
-
-       cursor_db.close()
-       try:
-           if pas[0][0] != Password:
-               return render_template('auth_bad.html')
-       except:
-           return render_template('auth_bad.html')
-
-       db_lp.close()
-       return render_template('successfulauth.html')
-
-   return render_template('authorization.html')
-
-@app.route('/registration', methods=['GET', 'POST'])
-def form_registration():
-
-   if request.method == 'POST':
-       Login = request.form.get('Login')
-       Password = request.form.get('Password')
-
-       db_lp = sqlite3.connect('login_password.db')
-       cursor_db = db_lp.cursor()
-       sql_insert = '''INSERT INTO passwords VALUES('{}','{}');'''.format(Login, Password)
-
-
-       cursor_db.execute(sql_insert)
-
-       cursor_db.close()
-
-       db_lp.commit()
-       db_lp.close()
-
-       return render_template('successfulregis.html')
-
-   return render_template('registration.html')
-
-
-if __name__ == "__main__":
- app.run()
+from flask import * 
+  
+app = Flask(__name__) 
+  
+@app.route('/admin') 
+def admin(): 
+    return 'admin' 
+  
+@app.route('/librarion') 
+def librarion(): 
+    return 'librarion' 
+  
+@app.route('/student') 
+def student(): 
+    return 'student' 
+  
+@app.route('/user/<name>') 
+def user(name): 
+    if name == 'admin': 
+        return redirect(url_for('admin')) 
+    if name == 'librarion': 
+        return redirect(url_for('librarion')) 
+    if name == 'student': 
+        return redirect(url_for('student')) 
+if __name__ =='__main__': 
+    app.run(debug = True) 
